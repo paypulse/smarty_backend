c6b9b554d6d3379541b8e859e8262b875b021626
smj1Grib <smj1@grib.co.kr>
2025-08-13 11:30:30 +0900

    [추가] build.gradle 파일 dependencies 추가 , Readme.md 에 프로젝트 관련 이미지 추가

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-#** Smarty Backend API **
+# ** Smarty Backend API **
+```
+    [개발 언어] :  kotlin 
+    [DB] :  postgres
+```
+
+# ** Smarty mind Map Info. **
+![My Logo](image/smarty_menu1.png)
+![My Logo](image/smarty_menu2.png)
+
+# ** Smarty table structure ** 
+### [추후 수정 진행 예정]
+![My Logo](image/smarty_diagram.png)
+
+
+# ** Smarty system structure ** 
+![My Logo](image/smarty_system_structure.png)
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("org.springframework.boot") version "3.5.4"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("plugin.jpa") version "1.9.25"
+    kotlin("kapt") version "1.9.24" // Q 클래스 생성 용
 }
 
 group = "kr.co.works"
@@ -27,20 +28,41 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    //spring security
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-
-    //LLM (spring AI)
-    implementation("org.springframework.ai:")
-
+    //DB
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("org.postgresql:postgresql")
+
+    //QueryDsl
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+
+    //kotlin
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    //LLM. Google gemini (vertex AI)
+    implementation("org.springframework.ai:spring-ai-starter-model-vertex-ai-gemini:1.0.1")
+    //크롤링
+    implementation("org.jsoup:jsoup:1.18.1")
+    //JWT 발급(서명)용
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+    // DEV /ops
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    //swegger / openAPI
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+
+    //logback
+    implementation("org.springframework.boot:spring-boot-starter-logging")
+
+    // test
     providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
@@ -54,12 +76,28 @@ kotlin {
     }
 }
 
+kapt {
+    correctErrorTypes = true
+    includeCompileClasspath = false  // 경고 제거
+}
+
+sourceSets {
+    named("main") {
+        java.srcDirs("build/generated/source/kapt/main")
+    }
+    named("test") {
+        java.srcDirs("build/generated/source/kapt/test")
+    }
+}
+
 allOpen {
     annotation("jakarta.persistence.Entity")
     annotation("jakarta.persistence.MappedSuperclass")
     annotation("jakarta.persistence.Embeddable")
 }
 
+
 tasks.withType<Test> {
     useJUnitPlatform()
+    enabled = false // 임시 비 활성 화
 }
